ebff7c3d424834b559b4cbaf145e8236cb4d2c0e
Alan Aasmaa <alanaasmaa@gmail.com>
2016-06-30 20:30:24 +0300

    Alkaa jo pikkuhiljaa näyttään sivulta

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,11 +8,18 @@
 @stop
 @section('content')
 <section class="container">
-    <div class="full box vcenter blue">
-        <div class="center">
-            <h5>SLOGAN</h5>
-            <h4 class="sticky">SLOGAN2</h4>
-            <p style="margin-top: 30px">Sit tähän lyhyesti musta ?Sit tähän lyhyesti musta ?Sit tähän lyhyesti musta ?Sit tähän lyhyesti musta ?</p>
+    <div class="full box blue vcenter homepage">
+        <div class="center whiteText">
+            <hr>
+            <h1>Teen sun duunit</h1>
+            <h5>branding • web design • development • internet marketing</h5>
+            <p>Sit tähän lyhyesti musta ?Sit tähän lyhyestagdi musta ?
+            Sit tähän lyhyesti musta ?Sit tähän lyhyesti musta ?</p>
+            <hr>
+            <p>
+                <a class="button" href="/">Portfolio</a>
+                <a class="button" href="/">Contact Us</a>
+            </p>
         </div>
     </div>
 </section>
