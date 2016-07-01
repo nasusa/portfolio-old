9f06a662abee82289d4a72b8eb6633e6d1fa94a1
Alan Aasmaa <alanaasmaa@gmail.com>
2016-07-01 14:22:07 +0300

    First Page Done

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,16 +1,57 @@
 <footer class="container box blue whiteText">
     <div class="fourth">
-    <i class="fa fa-globe" aria-hidden="true"><p>Im From Estonia</p></i>
+        <ul>
+            <li>
+                <i class="fa fa-user" aria-hidden="true"></i>&nbsp; My Name is Alan Aasmaa
+            </li>
+            <li>
+                <i class="fa fa-globe" aria-hidden="true"></i>&nbsp; I'm From Estonia
+            </li>
+            <li>
+                <i class="fa fa-calendar-o" aria-hidden="true"></i>&nbsp; I'm 21 years old
+            </li>
+        </ul>
     </div>
     <div class="fourth">
-    2
+        <ul>
+            <li>
+                <i class="fa fa-archive" aria-hidden="true"></i>&nbsp; Over 10 projects
+            </li>
+            <li>
+                <i class="fa fa-group" aria-hidden="true"></i>&nbsp; Over 20 clients
+            </li>
+            <li>
+                <i class="fa fa-headphones" aria-hidden="true"></i>&nbsp; I ♥ Rap & Hip-Hop
+            </li>
+        </ul>
     </div>
     <div class="fourth">
-    3
+    <ul>
+            <li>
+                <i class="fa fa-steam" aria-hidden="true"></i>&nbsp; I'm at <a href="http://steamcommunity.com/id/thasmog">Steam</a>
+            </li>
+            <li>
+                <i class="fa fa-steam" aria-hidden="true"></i>&nbsp; I'm at <a href="http://steamcommunity.com/id/thasmog">Steam</a>
+            </li>
+            <li>
+                <i class="fa fa-steam" aria-hidden="true"></i>&nbsp; I'm at <a href="http://steamcommunity.com/id/thasmog">Steam</a>
+            </li>
+        </ul>
     </div>
     <div class="fourth e">
-    4
+    <ul>
+            <li>
+                <i class="fa fa-steam" aria-hidden="true"></i>&nbsp; I'm at <a href="http://steamcommunity.com/id/thasmog">Steam</a>
+            </li>
+            <li>
+                <i class="fa fa-youtube" aria-hidden="true"></i>&nbsp; Watch me from <a href="https://www.youtube.com/c/thasmog">YouTube</a>
+            </li>
+            <li>
+                <i class="fa fa-steam" aria-hidden="true"></i>&nbsp; I'm at <a href="http://steamcommunity.com/id/thasmog">Steam</a>
+            </li>
+        </ul>
     </div>
+    <hr>
     <div class="footerInfo">
         <a href="/terms-and-conditions">Terms of Use</a> . Copyright &copy; <?php echo date("Y") ?> Alan Aasmaa®
     </div>
