37f2b98df98214c91247660b625d335f9244909a
Alan Aasmaa <alanaasmaa@gmail.com>
2016-07-05 18:37:10 +0300

    Staring Portfolio

---
+++ database/migrations/2016_07_05_150247_create_portfolios_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('published')->default(0);
+            $table->string('image')->default('http://fakeimg.pl/300x200/');
+            $table->date('started')->default(0000-00-00);
+            $table->date('finished')->default(0000-00-00);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('portfolios');
+    }
+}
