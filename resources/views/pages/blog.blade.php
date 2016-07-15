5cfa58a2fa78970f09f17b09e63257ff772061ff
Alan Aasmaa <alanaasmaa@gmail.com>
2016-07-15 11:34:27 +0300

    Target blog item now highlites :)

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -7,22 +7,24 @@
 <div class="container">
     <section class="sideContent">
         @foreach ($articles as $article)
-        <div class="box">
-            <a href="{{url('article/'.$article->slug)}}">
-                <img src="{{$article->image}}" class="blog-img">
-                <h4>{{$article->title}}</h4>
-            </a>
-            <div class="post-meta">
-                <ul>
-                    <li><i class="fa fa-user"></i> {{$article->user->name}}</li>
-                    <li><i class="fa fa-clock-o"></i> {{$article->created_at->diffForHumans()}}</li>
-                    <li><i class="fa fa-folder"></i> <a href="/categories/{{ $article->category->slug }}">{{ $article->category->name }}</a></li>
-                    <li><i class="fa fa-tags"></i> @foreach($article->tags as $key => $tag)<a href="/tags/{{ $tag->slug }}"> {{ $tag->name }}</a>@endforeach</li>
-                </ul>
-            </div>
-            <p>{!! str_limit($article->body_html, $limit = 200, $end = '...') !!}</p>
+        <article id="{{$article->id}}"class="box">
+            <header>
+                <a href="{{url('article/'.$article->slug)}}">
+                    <img src="{{$article->image}}">
+                    <h4>{{$article->title}}</h4>
+                </a>
+                <div>
+                    <ul>
+                        <li><i class="fa fa-user"></i> {{$article->user->name}}</li>
+                        <li><i class="fa fa-clock-o"></i> <time datetime="{{$article->created_at}}">{{$article->created_at->diffForHumans()}}</time></li>
+                        <li><i class="fa fa-folder"></i> <a href="/categories/{{ $article->category->slug }}">{{ $article->category->name }}</a></li>
+                        <li><i class="fa fa-tags"></i> @foreach($article->tags as $key => $tag)<a href="/tags/{{ $tag->slug }}"> {{ $tag->name }}</a>@endforeach</li>
+                    </ul>
+                </div>
+            </header>
+            {!! str_limit($article->body_html, $limit = 200, $end = '...') !!}
             <a  class="btn btn-primary" href="{{url('article/'.$article->slug)}}">Read More</a>
-        </div>
+        </article>
         @endforeach
         <div class="box center">
             {!! $articles->render() !!}
