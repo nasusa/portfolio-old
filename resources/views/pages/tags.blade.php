f6743aa6f3524ee1338971ab7418e7855ad149be
Alan Aasmaa <alanaasmaa@gmail.com>
2016-07-15 09:33:31 +0300

    Updated tags user class

--- resources/views/pages/tags.blade.php
+++ resources/views/pages/tags.blade.php
@@ -14,7 +14,7 @@
             <div class="post-meta">
             <h4>{{$article->title}}</h4>
                 <ul>
-                    <li><i class="fa fa-user"></i> Admin</li>
+                    <li><i class="fa fa-user"></i> {{$article->user->name}}</li>
                     <li><i class="fa fa-clock-o"></i> {{$article->created_at->diffForHumans()}}</li>
                     <li><i class="fa fa-folder"></i> <a href="/categories/{{ $article->category->slug }}">{{ $article->category->name }}</a></li>
                     <li><i class="fa fa-tags"></i> @foreach($article->tags as $key => $tag)<a href="/tags/{{ $tag->slug }}"> {{ $tag->name }}</a>@endforeach</li>
