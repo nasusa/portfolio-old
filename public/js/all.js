53c07c97628be1598f5a409a0ca0375c214dbc1d
Alan Aasmaa <alanaasmaa@gmail.com>
2016-07-18 20:44:49 +0300

    Added Analytics

--- public/js/all.js
+++ public/js/all.js
@@ -1,2 +1,22 @@
-var $backToTop=$(".back-to-top");$backToTop.hide(),$(window).on("scroll",function(){$(this).scrollTop()>200?$backToTop.fadeIn():$backToTop.fadeOut()}),$backToTop.on("click",function(o){$("html, body").animate({scrollTop:0},500)}),$(document).ready(function(){$("#nav-trigger").click(function(){$("#nav").toggle("slow",function(){})})});
+var $backToTop = $(".back-to-top");
+$backToTop.hide();
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > 200) {
+    $backToTop.fadeIn();
+  } else {
+    $backToTop.fadeOut();
+  }
+});
+
+$backToTop.on('click', function(e) {
+  $("html, body").animate({scrollTop: 0}, 500);
+});
+$(document).ready(function(){
+    $( "#nav-trigger" ).click(function() {
+        $( "#nav" ).toggle( "slow", function() {
+            // Animation complete.
+        });
+    });
+});
+
 //# sourceMappingURL=all.js.map
