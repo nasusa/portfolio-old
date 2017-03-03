143790ae0cabab9e9a9bebd97f2d74bc9d800b30
Susanna Lepola <lepolsussu@gmail.com>
2017-03-03 10:41:28 +0200

    test

--- resources/views/layouts/partials/meta.blade.php
+++ resources/views/layouts/partials/meta.blade.php
@@ -3,6 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="@yield('description')">
 <meta name="keywords" content="@yield('keywords')">
-<meta name="author" content="Alan Aasmaa">  
+<meta name="author" content="Susanna Lepola">  
 {{--<link rel="icon" href="../../favicon.ico">--}}
 <meta name="csrf-token" content="{{ csrf_token() }}">
